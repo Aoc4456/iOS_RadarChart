99b8caf5deb5d22092b62d59377aa3ff4ada1903
Aoc4456 <sizu44567789@gmail.com>
2021-04-09 14:39:07 +0900

    適当なチャートを表示

--- RaderChart/ViewController.swift
+++ RaderChart/ViewController.swift
@@ -7,12 +7,48 @@
 //
 
 import UIKit
+import Charts
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var radarChart: RadarChartView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        // 1
+        let greenDataSet = RadarChartDataSet(
+            entries: [
+                RadarChartDataEntry(value: 210),
+                RadarChartDataEntry(value: 60.0),
+                RadarChartDataEntry(value: 150.0),
+                RadarChartDataEntry(value: 150.0),
+                RadarChartDataEntry(value: 160.0),
+                RadarChartDataEntry(value: 150.0),
+                RadarChartDataEntry(value: 110.0),
+                RadarChartDataEntry(value: 190.0),
+                RadarChartDataEntry(value: 200.0)
+            ]
+        )
+        let redDataSet = RadarChartDataSet(
+            entries: [
+                RadarChartDataEntry(value: 120.0),
+                RadarChartDataEntry(value: 160.0),
+                RadarChartDataEntry(value: 110.0),
+                RadarChartDataEntry(value: 110.0),
+                RadarChartDataEntry(value: 210.0),
+                RadarChartDataEntry(value: 120.0),
+                RadarChartDataEntry(value: 210.0),
+                RadarChartDataEntry(value: 100.0),
+                RadarChartDataEntry(value: 210.0)
+            ]
+        )
+
+        // 2
+        let data = RadarChartData(dataSets: [greenDataSet, redDataSet])
+
+        // 3
+        radarChart.data = data
     }
 
 
