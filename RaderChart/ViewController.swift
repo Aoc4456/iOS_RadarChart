67898a483811ce50260f20c7f6eda4adc6d5add9
Aoc4456 <sizu44567789@gmail.com>
2021-04-09 14:23:33 +0900

    Initial Commit

---
+++ RaderChart/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RaderChart
+//
+//  Created by aoshima on 2021/04/09.
+//  Copyright © 2021 aoshima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
