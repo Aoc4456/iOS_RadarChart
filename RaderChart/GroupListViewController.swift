340ed1b1df465bf75541aa50ab57ab0d9bd6372d
Aoshima <>
2021-04-10 20:07:05 +0900

    リストに適当なデータを表示

--- RaderChart/GroupListViewController.swift
+++ RaderChart/GroupListViewController.swift
@@ -8,14 +8,23 @@
 
 import UIKit
 
-class GroupListViewController: UIViewController {
+class GroupListViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
+        self.navigationItem.title = "グループ"
     }
 
-
+    // セルの数を返す
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    // セルを返す
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = "\(indexPath.row)m"
+        return cell
+    }
 }
 
