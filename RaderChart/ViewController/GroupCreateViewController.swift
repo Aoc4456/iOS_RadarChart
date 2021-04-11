4c55e516ed8624c7942e8696b27922aff44b91a2
Aoshima <>
2021-04-11 15:21:07 +0900

    閉じるボタンを追加

---
+++ RaderChart/ViewController/GroupCreateViewController.swift
@@ -0,0 +1,24 @@
+//
+//  GroupCreateViewController.swift
+//  RaderChart
+//
+//  Created by M Aoshima on 2021/04/11.
+//  Copyright © 2021 aoshima. All rights reserved.
+//
+
+import UIKit
+
+class GroupCreateViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationItem.title = "グループ作成"
+        let leftButton = UIBarButtonItem(title: "閉じる", style: UIBarButtonItem.Style.plain, target: self, action: #selector(onTapCloseButton(_:)))
+        self.navigationItem.leftBarButtonItem = leftButton
+    }
+    
+    @objc func onTapCloseButton(_ sender: UIBarButtonItem){
+        self.dismiss(animated: true, completion: nil)
+    }
+
+}
