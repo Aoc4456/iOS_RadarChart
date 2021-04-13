df3a99b9aec64858a07797c8cf162be1163efa80
Aoshima <>
2021-04-13 19:22:14 +0900

    ColorPickerで選択した色を反映する

--- RaderChart/ViewController/GroupCreateViewController.swift
+++ RaderChart/ViewController/GroupCreateViewController.swift
@@ -12,9 +12,11 @@ class GroupCreateViewController: UIViewController {
     
     private var selectedColor = UIColor.systemTeal
     private var colorPicker = UIColorPickerViewController()
-
+    @IBOutlet weak var colorPickerView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        // setup navigation item
         self.navigationItem.title = "グループ作成"
         let leftButton = UIBarButtonItem(title: "閉じる", style: UIBarButtonItem.Style.plain, target: self, action: #selector(onTapCloseButton(_:)))
         self.navigationItem.leftBarButtonItem = leftButton
@@ -23,5 +25,22 @@ class GroupCreateViewController: UIViewController {
     @objc func onTapCloseButton(_ sender: UIBarButtonItem){
         self.dismiss(animated: true, completion: nil)
     }
+    
+    @IBAction func onTapColorPickerView(_ sender: Any) {
+        colorPicker.supportsAlpha = true // あとでfalseにして何が違うか確認する
+        colorPicker.selectedColor = selectedColor
+        colorPicker.delegate = self
+        present(colorPicker, animated: true)
+    }
+}
 
+// カラーピッカーdelegate
+extension GroupCreateViewController: UIColorPickerViewControllerDelegate{
+    func colorPickerViewControllerDidSelectColor(_ viewController: UIColorPickerViewController) {
+        selectedColor = viewController.selectedColor
+        colorPickerView.backgroundColor = selectedColor
+    }
+    
+    func colorPickerViewControllerDidFinish(_ viewController: UIColorPickerViewController) {
+    }
 }
