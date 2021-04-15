fd95e3bcc3193a9cf3227d86344e6c9e0c253278
Aoshima <>
2021-04-15 20:33:04 +0900

    テスト用チャートにプロパティを設定

--- RaderChart/ViewController/TestPageViewController.swift
+++ RaderChart/ViewController/TestPageViewController.swift
@@ -20,7 +20,6 @@ class TestPageViewController: UIViewController {
     }
     
     private func setupChart(){
-        // 1
         let greenDataSet = RadarChartDataSet(
             entries: [
                 RadarChartDataEntry(value: 210),
@@ -34,22 +33,81 @@ class TestPageViewController: UIViewController {
                 RadarChartDataEntry(value: 200.0)
             ]
         )
-        let redDataSet = RadarChartDataSet(
-            entries: [
-                RadarChartDataEntry(value: 120.0),
-                RadarChartDataEntry(value: 160.0),
-                RadarChartDataEntry(value: 110.0),
-                RadarChartDataEntry(value: 110.0),
-                RadarChartDataEntry(value: 210.0),
-                RadarChartDataEntry(value: 120.0),
-                RadarChartDataEntry(value: 210.0),
-                RadarChartDataEntry(value: 100.0),
-                RadarChartDataEntry(value: 210.0)
-            ]
-        )
-
-        // 2
-        let data = RadarChartData(dataSets: [greenDataSet, redDataSet])
+        
+        /*
+         * 個別のチャートの設定
+         */
+        // 線の太さ
+        greenDataSet.lineWidth = 3
+        // 線の色
+        greenDataSet.colors = [UIColor.green]
+        // 塗りつぶしの色
+        greenDataSet.fillColor = UIColor.green
+        greenDataSet.drawFilledEnabled = true
+        greenDataSet.valueFormatter = DataSetValueFormatter()
+        
+        /*
+         * チャートグループの設定
+         * 中心から外に向かう線    = x軸 (xAxis)
+         * ウェブ同士を結ぶ内側の線 = y軸 (yAxis)
+         */
+        // 中心から外に向かう線の太さ (デフォルト = 1.5)
+        raderChart.webLineWidth = 3
+        // 中心から外に向かう線の色
+        raderChart.webColor = UIColor.green
+        // 内側の線の太さ (デフォルト = 0.75)
+        raderChart.innerWebLineWidth = 2
+        // 内側の線の色
+        raderChart.innerWebColor = UIColor.green
+        // 回転を許すか
+        raderChart.rotationEnabled = false
+        // 凡例表示
+        raderChart.legend.enabled = true
+        // タップして強調表示する機能をOFF
+        raderChart.highlightPerTapEnabled = false
+        
+        /*
+         * x軸 (中心から外に向かう線) の設定
+         */
+        let xAxis = raderChart.xAxis
+        // xAxis.label = グラフの外側に表示される項目名
+        xAxis.labelFont = .systemFont(ofSize: 15,weight: .bold)
+        xAxis.labelTextColor = .black
+        xAxis.xOffset = 20 // TODO ラベルとの距離が設定できるはずだが、できない
+        xAxis.yOffset = 20
+        xAxis.valueFormatter = XAxisFormatter()
+        
+        /*
+         * y軸 (ウェブ同士を繋ぐ線)の設定
+         */
+        let yAxis = raderChart.yAxis
+        yAxis.labelFont = .systemFont(ofSize: 10,weight: .light)
+        // 分割数
+        yAxis.labelCount = 6
+        // y軸の値を非表示にする (最大値と最小値のラベルのみ非表示にすることも可)
+        yAxis.drawTopYLabelEntryEnabled = false
+        yAxis.drawBottomYLabelEntryEnabled = false
+        // グラフの原点を、値のうち最小のものではなく0にする
+        yAxis.axisMinimum = 0
+        
+        let data = RadarChartData(dataSets: [greenDataSet])
         raderChart.data = data
     }
+}
+
+
+// RaderChartDataSetにセットするFormatter
+// 各項目の最終的な値を表示しないようにするため、""を返す
+class DataSetValueFormatter:IValueFormatter{
+    func stringForValue(_ value: Double, entry: ChartDataEntry, dataSetIndex: Int, viewPortHandler: ViewPortHandler?) -> String {
+        return ""
+    }
+}
+
+class XAxisFormatter:IAxisValueFormatter{
+    let titles = "ABCDEFGHI".map { "Party \($0)" }
+       
+       func stringForValue(_ value: Double, axis: AxisBase?) -> String {
+           titles[Int(value) % titles.count]
+       }
 }
