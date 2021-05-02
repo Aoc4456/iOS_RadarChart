4ab52c621527f4801d9500ba20903cbea5c8ed53
Aoc4456 <sizu44567789@gmail.com>
2021-05-02 21:05:58 +0900

    適当なレイアウトを仮表示

--- RaderChart/Screen/ChartCollectionView/ChartCollectionViewController.swift
+++ RaderChart/Screen/ChartCollectionView/ChartCollectionViewController.swift
@@ -22,6 +22,16 @@ class ChartCollectionViewController: UIViewController {
         // setup Presenter
         self.presenter = ChartCollectionPresenter(view: self)
         
+        
+        // setup CollectionView
+        collectionView.dataSource = self
+        collectionView.register(UINib(nibName: "ChartListCell", bundle: nil), forCellWithReuseIdentifier: "ListCell")
+        
+        // setup CollectionViewCell
+        let layout = UICollectionViewFlowLayout()
+//        layout.itemSize = CGSize(width: collectionView.frame.width, height: 100)
+//        collectionView.collectionViewLayout = layout
+        
         presenter.viewDidLoad()
     }
 
@@ -30,4 +40,15 @@ class ChartCollectionViewController: UIViewController {
 // Presenterからの描画指示
 extension ChartCollectionViewController:ChartCollectionPresenterOutput{
     
+}
+
+extension ChartCollectionViewController:UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ListCell", for: indexPath)
+        return cell
+    }
 }
