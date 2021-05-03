8b74b97901258f7590439003da9cd72f78153f71
Aoc4456 <sizu44567789@gmail.com>
2021-05-03 21:04:35 +0900

    スマホでは２列表示、タブレットでは３列表示にする

--- RaderChart/Component/ChartGridCell/ChartGridCell.swift
+++ RaderChart/Component/ChartGridCell/ChartGridCell.swift
@@ -9,16 +9,23 @@
 import UIKit
 
 class ChartGridCell: UICollectionViewCell {
-
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-
 }
 
 func getChartGridCellFlowLayout(view:UIView) -> UICollectionViewFlowLayout {
     let layout = UICollectionViewFlowLayout()
-    layout.itemSize = CGSize(width: view.frame.width / 3.1, height: view.frame.width / 3.1)
+    
+    let mainBoundWidth: CGFloat = UIScreen.main.bounds.size.width
+    // iPhone8 Plusが414  iPadMiniが768
+    if(mainBoundWidth < 700){
+        layout.itemSize = CGSize(width: view.bounds.width / 2.1, height: view.bounds.width / 2.1)
+    }else{
+        layout.itemSize = CGSize(width: view.bounds.width / 3.1, height: view.bounds.width / 3.1)
+    }
+    
     return layout
 }
