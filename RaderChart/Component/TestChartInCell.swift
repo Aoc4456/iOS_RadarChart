c0abad368f446d881bb81f1defa64dec2351c637
Aoc4456 <sizu44567789@gmail.com>
2021-05-03 19:47:33 +0900

    適当なチャートを仮に表示

---
+++ RaderChart/Component/TestChartInCell.swift
@@ -0,0 +1,40 @@
+//
+//  GroupCreateSampleChart.swift
+//  RaderChart
+//
+//  Created by M Aoshima on 2021/04/22.
+//  Copyright © 2021 aoshima. All rights reserved.
+//
+
+import Foundation
+import Charts
+
+class TestChartInCell:RadarChartView{
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        rotationEnabled = false
+        legend.enabled = false
+        highlightPerTapEnabled = false
+        
+        let xAxis = self.xAxis
+        xAxis.labelFont = .systemFont(ofSize: 11,weight: .light)
+        xAxis.valueFormatter = RowXAxisFormatter()
+        
+        let yAxis = self.yAxis
+        //分割数
+        yAxis.setLabelCount(6, force: true)
+        yAxis.drawLabelsEnabled = false
+        yAxis.axisMinimum = 0
+        yAxis.axisMaximum = 100
+        yAxis.axisRange = 100
+        yAxis.spaceMax = 0
+        yAxis.spaceMin = 0
+        
+        let data = MyChartUtil.getSampleChartData(color: .blue, numberOfItems: 6)
+        self.data = data
+    }
+}
