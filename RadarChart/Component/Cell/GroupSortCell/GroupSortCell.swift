dc1ad1febfd77f1836cc6537fec45b07a06dc0a0
Aoc4456 <sizu44567789@gmail.com>
2021-06-01 19:44:12 +0900

    セルのレイアウトを作成

---
+++ RadarChart/Component/Cell/GroupSortCell/GroupSortCell.swift
@@ -0,0 +1,27 @@
+//
+//  GroupSortCell.swift
+//  RadarChart
+//
+//  Created by M Aoshima on 2021/06/01.
+//  Copyright © 2021 aoshima. All rights reserved.
+//
+
+import UIKit
+
+class GroupSortCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    override var intrinsicContentSize: CGSize{
+        return CGSize(width: frame.width, height: 90)
+    }
+}
