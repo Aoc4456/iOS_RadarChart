cd475df9694a70ce2831340e07b7c9851da88da2
Aoc4456 <sizu44567789@gmail.com>
2021-06-19 20:21:23 +0900

    並び替えコントロールの表示

--- RadarChart/Screen/LabelSortView/LabelSortViewController.swift
+++ RadarChart/Screen/LabelSortView/LabelSortViewController.swift
@@ -11,7 +11,8 @@ import UIKit
 class LabelSortViewController: UIViewController {
     
     private var presenter:LabelSortPresenterInput!
-
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -20,6 +21,14 @@ class LabelSortViewController: UIViewController {
 
         let leftButton = UIBarButtonItem(title: "閉じる", style: UIBarButtonItem.Style.plain, target: self, action: #selector(onTapCloseButton(_:)))
         self.navigationItem.leftBarButtonItem = leftButton
+        
+        // setup tableView
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.rowHeight = 80
+        
+        // 並び替え
+        tableView.setEditing(true, animated: true)
     }
     
 
@@ -30,4 +39,39 @@ class LabelSortViewController: UIViewController {
 
 extension LabelSortViewController:LabelSortPresenterOutput{
     
+}
+
+extension LabelSortViewController:UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "labelSortCell", for: indexPath)
+        cell.textLabel!.text = "TODO[indexPath.row]"
+        cell.showsReorderControl = true
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        print("aaa")
+    }
+}
+
+extension LabelSortViewController:UITableViewDelegate{
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        return .none
+    }
+    
+    func tableView(_ tableView: UITableView, shouldIndentWhileEditingRowAt indexPath: IndexPath) -> Bool {
+        return false
+    }
 }
